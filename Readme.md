d36377245ac1c020bddb6c9c1769f8c7a2492694
veetance <divineokos@hotmail.com>
2021-04-12 23:54:12 -0600

    Final Cam tracker Project

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# FACE TRACK ASSIGNMENT
+
+This website is designed to be funny; it tracks your face and uses the data to update visuals on canvas.
+when you look or move your head up, down, left, and right,
+the abstract character face gets influenced and background changes colors.
+
+
+# Sources
+*[Link](https://p5js.org/) 
+*[Link](https://github.com/auduno/clmtrackr)<face tracker>
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 var vidcapture, ctracker, drawcanvas;
 
 
-var mouth,Leye, Reye;
+
+let posNotfound = false;
 
 
+
+
+
+// var mouth,Leye, Reye;
 // function preload() {
 // mouth = loadImage('imgs/mouth.png');
 // Leye  = loadImage('imgs/Left-eye.png');
@@ -11,7 +16,9 @@ var mouth,Leye, Reye;
 // }
 
 
-
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
 
 function setup() {
@@ -23,7 +30,7 @@ function setup() {
 
 //p5 method for  creating video stream
 	vidcapture = createCapture(VIDEO);
-	vidcapture.size(vidcapture.width*5,vidcapture.height*6);
+	vidcapture.size(vidcapture.width*2,vidcapture.height*3);
 	vidcapture.hide();
 
 // start the tracker
@@ -39,12 +46,14 @@ function setup() {
 }
 
 
+
+
 function draw() {
 
 
 	background(0,50);
-	translate(vidcapture.width,0)
-	scale(-1,1)
+	translate(vidcapture.width*2.32,100)
+	scale(-1.5,1.5)
    
 
     // image(vidcapture,0, 0);
@@ -55,36 +64,57 @@ function draw() {
 
     if (positions) {
 
+    	posNotfound === true;
+    
+
         // ctracker.draw(drawcanvas);
 
         
 
-        print('x:' + positions[27][0])
-        print('y:' + positions[27][1])
+        // print('x:' + positions[27][0])
+        // print('y:' + positions[27][1])
+
+        console.log('x:' + positions[33][0])
+       console.log('y:' + positions[33][1])
 
 
-        var r = map(positions[27][0],800,600, 0, 255)
-        var b = map(positions[27][1],200,400, 0, 255)
+
+
+
+        var r = map(positions[33][0],250,525, 0, 255)
+        var b = map(positions[33][1],122 ,200, 0, 255)
+
+        var m = map(positions[60][1],360,370, 0, 255)
+        var m2 = map(positions[60][0],222,333, 0, 255)
         // image(Leye, positions[27][0], positions[32][1]);
 
-        background(r*10,155,b*10)
-
+        
+		background(r, 155,r*2)
         
 
         fill(255);
-        ellipse(positions[27][0],positions[32][1],[40])
-        ellipse(positions[32][0],positions[27][1],[40])
+        ellipse(positions[27][0],positions[27][1],[50+(m/30)/10])
+        ellipse(positions[32][0],positions[32][1],[50+(m/30)/10])
 
 
 		fill(0);
-        ellipse(positions[27][0],positions[32][1],[10])
-        ellipse(positions[32][0],positions[27][1],[10])
+        ellipse(positions[27][0],positions[27][1],[20 - (m /-290) ])
+        ellipse(positions[32][0],positions[32][1],[20 - (m /-290) ])
 
 		fill(252,3,123);
-        ellipse(positions[60][0],positions[60][1],[r/3])
-
+        ellipse(positions[57][0],positions[57][1],[40+(m/55.5)])
 
     }
+    
+     else if  (posNotfound === false) {
+
+     	background(60,155,130)
+	
+    fill(255,5,84,10);
+ 	ellipse(290,214, 50, 50);
+    }
+
+
 }
 
 
@@ -98,27 +128,7 @@ function draw() {
 
 
 
-	// translate( width/2, height/2);
-	
-	// noStroke();
-	// fill(255,203,0);
-	// // circle( mouseX, mouseY, 40);
-
-	// // print("x: " + mouseX)
-	// // print("y: " + mouseY)
 
 
-
-	// var xColor = map(mouseX, 0, width, 0, 255)
-	// var yColor = map(mouseY, 0, height, 0, 255)
-
-	// stroke(xColor, 0 ,yColor);
-
-	// var xRotate = map(mouseX, 0, width, 0, 360)
-	// print(xRotate)
-	// angleMode(DEGREES)
-	// rotate(xRotate);
-	// // line(mouseX + 50, mouseY + 50, mouseX - 50, mouseY - 50)
-	// line(0,0,50,50)
 
 
